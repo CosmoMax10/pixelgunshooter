ee474cccb7f7cb7fc65360f4a4d7b6400ec7de7b
CosmoMax10 <cosmomax10@gmail.com>
2022-11-07 15:25:55 +0900

    added project

---
+++ Assets/Scripts/TakingDamage.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class TakingDamage : MonoBehaviourPunCallbacks
+{   
+
+    [SerializeField]
+    Image healthBar;
+
+    public float health;
+    public float startHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = startHealth;
+
+        healthBar.fillAmount = health/startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [PunRPC]
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log(health);
+
+        healthBar.fillAmount = health/startHealth;
+
+        if (health<=0)
+        {
+            //Die
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if(photonView.IsMine)
+        {
+            PixelGunGameManager.instancce.LeaveRoom();
+        }
+    }
+}
